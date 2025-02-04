559d14a441c441503b91cfd3b1f7f0364d3df4ee
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2025-02-04 00:12:51 -0300

    Add product to cart Function and update

--- script.js
+++ script.js
@@ -13,17 +13,17 @@ const cartCount = document.getElementById("cart-count");
 const addressInput = document.getElementById("address");
 const addressWarn = document.getElementById("address-warn");
 
+let cart = [];
 
-//functions para abrir carrinho
 
-
+//function para abrir carrinho
 cartBtn.addEventListener("click", function(){
+    updateCartModal()
    cartModal.style.display = "flex";
+   
 })
 
-
-//functions para fechar carrinho
-
+//function para fechar carrinho
 cartModal.addEventListener("click", function(event) {
     if(event.target === cartModal) {
         
@@ -33,8 +33,71 @@ cartModal.addEventListener("click", function(event) {
     })
 
 
-    closeModal.addEventListener("click", function(){
+closeModal.addEventListener("click", function(){
         cartModal.style.display = "none";
     })
+
+
+    //implementando adicionar ao carrinho
+    menu.addEventListener("click", function(event){
+
+        let parentButton = event.target.closest(".add-to-cart-btn");
+
+        if(parentButton){
+           const name = parentButton.getAttribute("data-name");
+           const price = parentButton.getAttribute("data-price");
+            AddToCart(name,price);
+        }
+
+    });
+
+
+    function AddToCart(name, price){
+
+        const existingItem = cart.find(item => item.name === name);
+
+        if(existingItem){
+            existingItem.quantity += 1;
+            
+        }else{
+        cart.push({
+                name,
+                price,
+                quantity: 1 })
+    }
+
+    updateCartModal()
+    
+    cartModal.style.display = "flex";
+    }
+
+function updateCartModal(){
+
+    cartItensConteiner.innerHTML="";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+
+        cartItemElement.classList.add("flex","justify-between", "mb-4", "flex-col")
+        cartItemElement.innerHTML =`
+        <div class="flex items-center justify-between">
+         <div >
+          <p class="font-medium">${item.name}</p>
+          <p>Qtd: ${item.price}</p>
+          <p class="font-medium mt-2">(${item.quantity})</p>
+         </div>
+         
+        <div>
+        <button class="font-bold"><i class="fa-solid fa-trash"></i><button>
+        </div>
+        </div>
+        `
+
+        cartItensConteiner.appendChild(cartItemElement);
+    })
+    
+
+}
 
 
